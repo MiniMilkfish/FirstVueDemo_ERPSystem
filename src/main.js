70781b78772d91a62429fa2a2dc5d7e58aa73271
minimilkfish <minimilkfish@outlook.com>
2021-12-23 10:46:17 +0800

    #IMPROVED 入口函数

--- src/main.js
+++ src/main.js
@@ -1,8 +1,58 @@
-import { createApp } from 'vue'
-// import Button from 'ant-design-vue/lib/button';
+import { createApp } from 'vue';
+// import { createStore } from 'vuex';
 import 'ant-design-vue/dist/antd.css';
-// import zhCN from 'ant-design-vue/lib/locale-provider/zh_CN';
-import App from './App.vue'
-// import {createStore} from 'store';
+import './assets/fonts/font.css';
+import {
+  Layout,
+  Menu,
+  Breadcrumb,
+  Input,
+  Form,
+  Checkbox,
+  Button,
+  Spin,
+  Dropdown
+} from 'ant-design-vue';
+import App from './App.vue';
+import router from './route/index';
+// import MOTATION_TYPES from './store/constantMotationTypes';
+import store from './store/index';
 
-createApp(App).mount('#app');
+import vMasterDashboard from "./components/vMasterDashboard.vue";
+import vNotFound from "./components/vNotFound.vue";
+
+const app = createApp(App);
+
+/**
+ * 路由引用
+ */
+app.use(router);
+
+/**
+ * 状态管理引用
+ */
+app.use(store);
+
+/**
+ * ant-design-vue 组件引用
+ */
+app.use(Layout)
+  .use(Menu)
+  .use(Breadcrumb)
+  .use(Input)
+  .use(Form)
+  .use(Checkbox)
+  .use(Spin)
+  .use(Dropdown)
+  .use(Button);
+
+/**
+ * 注册自定义组件
+ */
+app.component('vMasterDashboard', vMasterDashboard);
+app.component('vNotFound', vNotFound);
+
+/**
+ * 应用挂载
+ */
+app.mount('#app');
