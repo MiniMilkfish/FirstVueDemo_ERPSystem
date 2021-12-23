8c0bee84a6e04cf3f126c92db718b900fffa52e8
minimilkfish <minimilkfish@outlook.com>
2021-12-23 10:28:30 +0800

    #IMPROCED 追加vue-cli 环境变量配置

--- vue.config.js
+++ vue.config.js
@@ -1,18 +1,9 @@
-// vue.config.js for less-loader@6.0.0
+
 module.exports = {
-    // css: {
-    //   loaderOptions: {
-    //     less: {
-    //       lessOptions: {
-    //         // If you are using less-loader@5 please spread the lessOptions to options directly
-    //         modifyVars: {
-    //           'primary-color': '#1DA57A',
-    //           'link-color': '#1DA57A',
-    //           'border-radius-base': '2px',
-    //         },
-    //         javascriptEnabled: true,
-    //       },
-    //     },
-    //   },
-    // },
-};
+    chainWebpack: config => {
+        config.plugin('html').tap(args => {
+            args[0].title = "内部运维系统平台 | Internal Operation & Maintenance System Platform";
+            return args;
+        })
+    }
+}
