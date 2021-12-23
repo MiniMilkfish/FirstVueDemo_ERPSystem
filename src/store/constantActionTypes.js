60f6fe11c1f0603246b35507fe4bb8f555da99d6
minimilkfish <minimilkfish@outlook.com>
2021-12-23 10:45:06 +0800

    #IMPROVED 操作类型管理

---
+++ src/store/constantActionTypes.js
@@ -0,0 +1,10 @@
+/**
+ * Action 事件类型
+ */
+export default {
+    INCREMENT: 'INCREMENT',
+    LOGIN_INCREMENT: 'LOGIN_INCREMENT',
+
+    // 登录页
+    LOGIN_FORM_SUBMIT: 'LOGIN_FORM_SUBMIT',                                  // 登录窗体提交
+};
---
+++ src/store/constantMotationTypes.js
@@ -0,0 +1,17 @@
+/**
+ * Motation 事件类型
+ */
+export default {
+    INCREMENT: 'INCREMENT',
+    LOGIN_INCREMENT: 'LOGIN_INCREMENT',
+
+    // 平台头部
+    MASTER_HEADER_TAB_ON_SELECT: 'MASTER_HEADER_TAB_ON_SELECT',              // 头Tab页选中事件
+
+    // 平台左侧菜单
+    MASTER_LEFT_MENU_ITEM_ON_CLICK: 'MASTER_LEFT_MENU_ITEM_ON_CLICK',      //  左侧菜单菜单项点击事件
+
+    // 登录页
+    WINDOWS_ON_RESIZE: 'WINDOWS_ON_RESIZE',                                  // 窗体窗口调整事件
+    LOGIN_FORM_SUBMIT: 'LOGIN_FORM_SUBMIT',                                  // 登录窗体提交
+};
