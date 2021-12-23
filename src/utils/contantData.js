978302392c8a654bcbfe29f1753d26fa940aa8e7
minimilkfish <minimilkfish@outlook.com>
2021-12-23 10:39:07 +0800

    #IMPROVED 存储全局常量数据

---
+++ src/utils/contantData.js
@@ -0,0 +1,38 @@
+/**
+ * 全局常量
+ */
+export default {
+    // 全局通知通知类型
+    NOTIFICATION_TYPES: {
+        SUCCESS: 'success',
+        INFO: 'info',
+        ERROR: 'error',
+        WARNING: 'warning'
+    },
+    // 静态页列表
+    PAGE_LISTS: {
+        'DASHBOARD': 'vMasterDashboard',   // 首页
+        // 'MARKETING_PLAN': 'marketingPlan',      // 销售合同
+        'MARKETING_CONTRACT': 'marketingContract',    // 销售 - 销售合同
+        'CUSTOMER_CLASSIFICATION': 'customerClassification',   // 客户分类
+        'CUSTOMER_LIST': 'customerList',       // 客户列表
+        'OPERATION_AND_MAINTENANCE_TIME': 'operationAndMaintenanceTime',     //运维时间
+        'UNIT_OF_MEASUREMENT': 'unit of measurement', // 计量单位
+        'INVENTORY_ALARM_SETTINGS': 'Inventory alarm settings',    // 库存报警设置
+        'SUPPLY_CHANNEL': 'Supply channel', // 供应渠道
+        // 'INVENTORY_SCHEDULING': 'Inventory Scheduling',     //库存调度
+        'STOCK_IN_AND_STOCK_OUT': 'Stock in and stock out',     // 入库出库
+        'PRODUCT_MANAGEMENT': 'Product management',     // 产品管理
+        'ACCESSORIES_MANAGEMENT': 'Accessories management',     // 配件管理
+        'INVENTORY_COUNT': 'Inventory count',   // 库存盘点
+        'BUSINESS_MARKETING_CONTRACT': 'Business  marketing_contract',     //商务 - 销售合同
+        'VERIFICATION_REPORT': 'Verification report',   //校验报告
+        'CONTROL_OVER_INVOICES': 'control over invoices',       //发票管理
+        'PRODUCTION_SCHEDULE': 'Production schedule',   //生产计划表
+        'DISPATCH_LIST': 'Dispatch list',       // 派单列表
+        'PERSONNEL_LIST': 'Personnel list',     // 人员列表
+        'BUILDING_SITE_LIST': 'building site list',     // 工地列表
+
+
+    }
+}
