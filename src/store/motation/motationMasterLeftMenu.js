d38ed9034b56e66e5c2cc9190770223fc6843712
minimilkfish <minimilkfish@outlook.com>
2021-12-29 09:27:25 +0800

    #IMPROVED 左侧菜单栏点击事件逻辑调整

--- src/store/motation/motationMasterLeftMenu.js
+++ src/store/motation/motationMasterLeftMenu.js
@@ -4,8 +4,25 @@ import MOTATION_TYPES from '../constantMotationTypes';
  * 处理同步请求
  */
 export default {
+    /**
+     * 左侧菜单栏点击事件
+     * @param {Object} state 
+     * @param {Object} payload 
+     * @param {String} payload.key
+     * @param {Array} payload.keyPath
+     * @param {Array} payload.rootKey
+     */
     [MOTATION_TYPES.MASTER_LEFT_MENU_ITEM_ON_CLICK](state, payload) {
-        state.menuRootPath = payload.keyPath;
+        let targetPath = payload.key.split("_");
+        if (targetPath[1] === void 0 || targetPath[1] === 'undefined') targetPath[1] = '';
+        let keyPaths = payload.keyPath;
+        keyPaths[keyPaths.length - 1] = targetPath[0];
+
+        state.menuRootPath = keyPaths;
         state.menuRoot = payload.rootKey;
+        state.currentMenuPageUrl = targetPath[1];
+    },
+    [MOTATION_TYPES.CLEAR_CURRENT_MENU_PAGE_URL](state) {
+        state.currentMenuPageUrl = '';
     }
 }
