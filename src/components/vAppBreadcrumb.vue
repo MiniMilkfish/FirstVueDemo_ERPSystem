180032bf494792ab290de8332ecbdde2affcdc00
minimilkfish <minimilkfish@outlook.com>
2022-01-17 18:53:39 +0800

    #IMPROVED 优化面包屑显/隐逻辑

--- src/components/vAppBreadcrumb.vue
+++ src/components/vAppBreadcrumb.vue
@@ -1,7 +1,9 @@
 <!-- 路径 面包屑组件 -->
 <template>
-  <a-breadcrumb v-if="breadShow" style="margin-bottom: 20px;">
-    <a-breadcrumb-item v-for="(title, index) in breadCrumbs" :key="index">{{title}}</a-breadcrumb-item>
+  <a-breadcrumb v-if="breadShow" style="margin-bottom: 20px">
+    <a-breadcrumb-item v-for="(title, index) in breadCrumbs" :key="index">{{
+      title
+    }}</a-breadcrumb-item>
   </a-breadcrumb>
 </template>
 <script>
@@ -10,14 +12,33 @@
 
   export default defineComponent({
     computed: {
-      breadCrumbs: function(){
+      breadCrumbs: function () {
         let keyPaths = this.$store.state.moduleMasterLeftMenu;
-        return keyPaths.menuRoot.concat(keyPaths.menuRootPath)
+        return keyPaths.menuRoot.concat(keyPaths.menuRootPath);
       },
-      breadShow(){
-        return FAKE_DATE.routeList.filter(item => item.title === this.$store.state.moduleMasterHeader.currentHeaderTabTag && item.sub.length > 0)[0]
-      }
-    }
+      breadShow() {
+        /**
+         * 面包屑显/隐逻辑
+         * 1、当前Tab 存在默认显示页时，关闭显示；
+         * 2、当前Tab 不存在默认显示页，但是不存在子集时, 关闭显示；
+         */
+        let _this = this,
+          tabPageUrl =
+            this.$store.state.moduleMasterHeader
+              .currentHeaderTabDashboardPageUrl;
+
+        if (tabPageUrl && tabPageUrl.length > 0) {
+          return false;
+        } else {
+          return FAKE_DATE.routeList.filter(
+            item =>
+              item.title ===
+                _this.$store.state.moduleMasterHeader.currentHeaderTabTag &&
+              item.sub.length > 0
+          )[0];
+        }
+      },
+    },
   });
 </script>
 <style></style>
