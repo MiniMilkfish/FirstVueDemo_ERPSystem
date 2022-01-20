9b0bfda7e30defa03cf68c6b87dbdd1e59202474
minimilkfish <minimilkfish@outlook.com>
2022-01-20 09:38:45 +0800

    #IMPROVED Store 加入Global组件、计量单位组件

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,17 @@
 import { createStore, createLogger } from 'vuex';
+import moduleGlobal from './module/moduleGlobal';
 import moduleLogin from './module/moduleLogin';
 import moduleMasterHeader from './module/moduleMasterHeader';
 import moduleMasterLeftMenu from './module/moduleMasterLeftMenu';
+import moduleUnitOfMeasurement from './module/moduleUnitOfMeasurement';
 
 export default createStore({
     modules: {
+        moduleGlobal,
         moduleLogin,
         moduleMasterHeader,
-        moduleMasterLeftMenu
+        moduleMasterLeftMenu,
+        moduleUnitOfMeasurement,
     },
     plugins: [createLogger()]
 });
