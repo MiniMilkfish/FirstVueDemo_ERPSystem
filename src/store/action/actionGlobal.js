8faec891108ee58a0f0d4fa807694f1e9dfbe5c4
minimilkfish <minimilkfish@outlook.com>
2022-01-20 09:36:34 +0800

    #IMPROVED Global Action

---
+++ src/store/action/actionGlobal.js
@@ -0,0 +1,9 @@
+// import ACTION_TYPES from '../constantActionTypes';
+// import ServerConfig from '../../config/urlConfig';
+// import * as common from '../../utils/common';
+
+/**
+ * 处理异步请求
+ */
+export default {
+}
