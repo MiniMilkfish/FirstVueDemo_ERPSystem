4c68ed5325e95a252f3f95e3427db0fa0241dcc3
minimilkfish <minimilkfish@outlook.com>
2022-01-20 09:33:08 +0800

    #IMPROVED 追加Global 状态管理，包含全局提醒、Loading

---
+++ src/store/state/stateGlobal.js
@@ -0,0 +1,11 @@
+import CONSTANT_DATA from '../../utils/contantData';
+
+export default {
+    notification: {
+        message: "",
+        description: "",
+        type: CONSTANT_DATA.NOTIFICATION_TYPES.ERROR,
+        tick: 1,
+    },
+    spinning: false
+};
